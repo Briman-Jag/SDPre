c1c98931ac2b3db993cfdbd731a711087e6710f9
Briman-Jag <brianstreetman25@gmail.com>
2019-08-25 15:39:23 -0700

    JavaTheHardWay Exercises 1-3 Complete

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main ( String[] args) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|     CORNER STORE       |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| 2019-8-25  02:55PM     |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:      10.870   |" );
+    System.out.println( "| Price/gallon: $ 3.529  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel total:  $ 38.36   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
