ead2a5177244a019e06c0173621d1a5d0c18496f
Briman-Jag <brianstreetman25@gmail.com>
2019-08-25 15:10:38 -0700

    JavaTheHardWay Exercises

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+    public static void main( String[] args) {
+      System.out.println( "I, Brian, am determined to learn how to code.");
+      System.out.println( "Today's date is August 25, 2019");
+    }
+}
