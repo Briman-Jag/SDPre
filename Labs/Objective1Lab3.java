7b97c391b14c74cdde3079756fb2f7eaff637d6e
Briman-Jag <brianstreetman25@gmail.com>
2019-08-25 19:44:00 -0700

    Completed Objective1Lab3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,10 @@
+public class Objective1Lab3 {
+
+  public static void main(String[] args){
+
+    System.out.println("Name: Brian S");
+    System.out.println("Age: 28");
+    System.out.println("State: Washington");
+
+  }
+}
