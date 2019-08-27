f3d5de3898a8e84e23913f059d60efe258ee6627
Briman-Jag <brianstreetman25@gmail.com>
2019-08-26 23:38:12 -0700

    Completed Objective2Lab1

--- Labs/Objective2Lab1.java
+++ Labs/Objective2Lab1.java
@@ -1,6 +1,6 @@
 public class Objective2Lab1 {
   public static void main ( String [] args ) {
-    Sting Hello = "Hello World";
+    String Hello = "Hello World";
 
     System.out.println( Hello );
   }
