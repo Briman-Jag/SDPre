7ba28e58a9b0c3533ae00b5f26f8a22b20975981
Briman-Jag <brianstreetman25@gmail.com>
2019-08-26 23:42:21 -0700

    Completed Objective2Lab

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main ( String [] args ) {
+    String Name = "Brian S";
+    int age = 28;
+    String State = "Washington";
+
+    System.out.println( "Name: " + Name + " Age: " + age + " State: " + State );
+  }
+}
